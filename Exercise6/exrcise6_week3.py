cf482ad63b3208239f7af7d8208026fb61a4b568
MagnusHansson <magnus.p.hansson@gmail.com>
2025-01-25 14:22:49 +0100

    Added exercise 6

---
+++ Exercise6/exrcise6_week3.py
@@ -0,0 +1,30 @@
+todo_list = []
+done_list = []
+
+while True:
+    print("1. Se innehållet i din lista")
+    print("2. Lägg till nya punkter i din lista")
+    print("3. Markera som klar.")
+    print("4. Klargjorda saker.")
+    alternativ = input("Välj ett altenativ: ")
+    try:
+        alternativ = int(alternativ)
+        if alternativ == 1:
+            print(todo_list)
+        elif alternativ == 2:
+            add_to_list = input("Skriv in en ny sak du måste komma ihåg att göra: ")
+            todo_list.append(add_to_list)
+            print(f"Ok, lade till {add_to_list} i listan.")
+        elif alternativ == 3:
+            remove_from_list = input("Vilken sak är du klar med? ")
+            todo_list.remove(remove_from_list)
+            done_list.append(remove_from_list)
+            print(f"Ok, tog bort {remove_from_list} från listan.")
+        elif alternativ == 4:
+                print(done_list)
+        else:
+            print("Ogiltigt värde, försök igen.")
+    except ValueError:
+        print("Det är inget giltigt alternativ, försök igen")
+
+

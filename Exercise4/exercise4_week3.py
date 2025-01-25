4e0fdf8ed67357a6081fc8adb94ca43cedf92ab5
MagnusHansson <magnus.p.hansson@gmail.com>
2025-01-25 14:32:24 +0100

    Added more in exercise 4

--- Exercise4/exercise4_week3.py
+++ Exercise4/exercise4_week3.py
@@ -1,48 +1,50 @@
 # # Del a
-# for y in range(1, 7):
-#     s = ""
-#     for x in range(1, 9):
-#         if x == 1:
-#             s += "#"
-#         else:
-#             s += "."
-#     print(s)
-#
+for y in range(1, 7):
+    s = ""
+    for x in range(1, 9):
+        if x == 1:
+            s += "#"
+        else:
+            s += "."
+    print(s)
+
 # Del b
-# for y in range(1, 7):
-#     s = ""
-#     for x in range(1, 9):
-#         if x == y:
-#             s += "#"
-#         else:
-#             s += "."
-#     print(s)
-#
+for y in range(1, 7):
+    s = ""
+    for x in range(1, 9):
+        if x == y:
+            s += "#"
+        else:
+            s += "."
+    print(s)
+
 # Del c
-# for y in range(1, 7):
-#     s = ""
-#     for x in range(1, 9):
-#         if 3 <= x <= 5:
-#             s += "#"
-#         else:
-#             s += "."
-#     print(s)
-#
+for y in range(1, 7):
+    s = ""
+    for x in range(1, 9):
+        if 3 <= x <= 5:
+            s += "#"
+        else:
+            s += "."
+    print(s)
+
 # Del d
-# for y in range(1, 7):
-#     s = ""
-#     for x in range(1, 9):
-#         if x == 3 or y == 3:
-#             s += "#"
-#         else:
-#             s += "."
-#     print(s)
-#
- Del e
- for y in range(1, 7):
+for y in range(1, 7):
+    s = ""
+    for x in range(1, 9):
+        if x == 3 or y == 3:
+            s += "#"
+        else:
+            s += "."
+    print(s)
+
+#Del e
+for y in range(1, 7):
      s = ""
      for x in range(1, 9):
          if x == 5:
+             s += "#"
+         elif x == 7 - y:
              s += "#"
          else:
              s += "."
@@ -52,7 +54,7 @@
 for y in range(1, 7):
     s = ""
     for x in range(1, 9):
-        if x == y:
+        if x == y or x == 7 - y:
             s += "#"
         else:
             s += "."

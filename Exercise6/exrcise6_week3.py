c1cbca51a2e53ec8455158e143992e0e92d99670
MagnusHansson <magnus.p.hansson@gmail.com>
2025-01-25 14:41:50 +0100

    Updated exercise 6

--- Exercise6/exrcise6_week3.py
+++ Exercise6/exrcise6_week3.py
@@ -6,11 +6,16 @@ while True:
     print("2. Lägg till nya punkter i din lista")
     print("3. Markera som klar.")
     print("4. Klargjorda saker.")
+    print("5. Avsluta.")
     alternativ = input("Välj ett altenativ: ")
     try:
         alternativ = int(alternativ)
         if alternativ == 1:
-            print(todo_list)
+            if len(todo_list) > 0:
+                for todo in todo_list:
+                    print("+ " + todo)
+            else:
+               print("Listan är tom")
         elif alternativ == 2:
             add_to_list = input("Skriv in en ny sak du måste komma ihåg att göra: ")
             todo_list.append(add_to_list)
@@ -21,7 +26,13 @@ while True:
             done_list.append(remove_from_list)
             print(f"Ok, tog bort {remove_from_list} från listan.")
         elif alternativ == 4:
-                print(done_list)
+            if len(done_list) > 0:
+                for done in done_list:
+                    print("+ " + done)
+            else:
+                print("Listan är tom")
+        elif alternativ == 5:
+            break
         else:
             print("Ogiltigt värde, försök igen.")
     except ValueError:

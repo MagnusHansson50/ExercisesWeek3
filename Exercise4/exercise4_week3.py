7df79b9ca80f4ab008b67ceecbc8f2657868561e
MagnusHansson <magnus.p.hansson@gmail.com>
2025-01-25 14:04:35 +0100

    Commit for exercise 4 and 5. Exercise 4 not ready.

---
+++ Exercise4/exercise4_week3.py
@@ -0,0 +1,59 @@
+# # Del a
+# for y in range(1, 7):
+#     s = ""
+#     for x in range(1, 9):
+#         if x == 1:
+#             s += "#"
+#         else:
+#             s += "."
+#     print(s)
+#
+# Del b
+# for y in range(1, 7):
+#     s = ""
+#     for x in range(1, 9):
+#         if x == y:
+#             s += "#"
+#         else:
+#             s += "."
+#     print(s)
+#
+# Del c
+# for y in range(1, 7):
+#     s = ""
+#     for x in range(1, 9):
+#         if 3 <= x <= 5:
+#             s += "#"
+#         else:
+#             s += "."
+#     print(s)
+#
+# Del d
+# for y in range(1, 7):
+#     s = ""
+#     for x in range(1, 9):
+#         if x == 3 or y == 3:
+#             s += "#"
+#         else:
+#             s += "."
+#     print(s)
+#
+ Del e
+ for y in range(1, 7):
+     s = ""
+     for x in range(1, 9):
+         if x == 5:
+             s += "#"
+         else:
+             s += "."
+     print(s)
+
+# Del f
+for y in range(1, 7):
+    s = ""
+    for x in range(1, 9):
+        if x == y:
+            s += "#"
+        else:
+            s += "."
+    print(s)
---
+++ Exercise5/exercise5_week3.py
@@ -0,0 +1,25 @@
+import random
+
+secret = random.randint(1, 100)
+antal_gissningar = 0
+
+while True:
+    gissning = input("Välkommen till gissa talet! Jag tänker på ett tal mellan 1 och 100. Kan du gissa vilket det är? ")
+
+    try:
+        gissning = int(gissning)
+        antal_gissningar += 1
+
+        if gissning < secret:
+            print("Nej, det är för lågt.")
+            if abs(gissning - secret) <= 5:
+                print("Det börjar brännas!!!")
+        elif gissning > secret:
+            print("Nej, det är för högt.")
+            if abs(gissning - secret) <= 5:
+                print("Det börjar brännas!!!")
+        else:
+            print(f"Det är rätt!!! Du gjorde det på {antal_gissningar} gissningar.")
+            break
+    except ValueError:
+        print("Det är inget tal, försök igen")
